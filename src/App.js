a250c45cbf92c1c5c6a0faefbbcb9c0181794736
Anet <Anet@A33COMP14.step.itstep.rv.ua>
2019-04-03 16:32:46 +0300

    ListItem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let todoItems=[];
+todoItems.push({index:1,value:"To do home task from Math",done:false});
+todoItems.push({index:2,value:"To do home task from Programmer",done:false});
+todoItems.push({index:3,value:"Solve all problems in a hole world!",done:false});
+todoItems.push({index:4,value:"Become older",done:true});
+
+
+class TodoList extends Component{
+ render(){
+   var todoItems = this.props.items.map((item,index) =>{
+       return ( <TodoListItem key={index} item={item} index={index} removeItem={this.props.removeItem} markDone={this.props.markDone}/>);
+   });
+   return (
+     <ul className="list-group">{todoItems}</ul>
+   );
+ }
+
+}
+
+class TodoListItem extends Component{
+  constructor(props){
+    super(props);
+    this.onClickClose=this.onClickClose.bind(this);
+    this.onClickDone=this.onClickDone.bind(this);
+
+  }
+  onClickClose(){
+var index=parseInt(this.props.index);
+this.props.removeItem(index);
+  }
+  onClickDone(){
+
+  }
+  render(){}
+}
+
+class TodoForm extends Component{
+  render(
+
+  )
+}
+
+class TodoHeader extends Component{
+  render(){
+    return  <h1>Hello Anet from TODO list!</h1>
+  }  
+  
+}
+
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+       {/* load our TODO list */}
       </div>
     );
   }
